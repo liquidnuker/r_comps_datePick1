ae2b16f5efa9967938824470ba466d57a85f9ddf
administrator <liquidnuker@users.noreply.github.com>
2017-07-19 10:04:24 -0700

    initial commit

---
+++ src/datePick1.js
@@ -0,0 +1,6 @@
+// flatpickr
+// ======================================================/
+const Flatpickr = require("flatpickr");
+
+const myInput = document.querySelector(".flatpickr1");
+const fp = new Flatpickr(myInput, {});  // Flatpickr
